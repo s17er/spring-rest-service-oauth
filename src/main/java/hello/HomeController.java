2af4ebb1fc7f26b704a96abd11fe1e010deaec1a
sasaki <sasaki@whizz-tech.co.jp>
2015-03-16 17:06:43 +0900

    OAuthの認証を確認するための画面を実装

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -16,15 +16,20 @@
 
 package hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
-	@RequestMapping("/")
-	public String home() {
-		return "home";
+	@RequestMapping(method = RequestMethod.GET)
+	public String home(Model model) {
+		return "home/index";
 	}
 
 }
